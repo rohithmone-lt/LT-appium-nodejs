de4d77eab50bf1bb93a0eb091f1e4519d56356db
Rohith Mone <98173597+rohithmone-lt@users.noreply.github.com>
2022-08-18 13:41:21 +0530

    Update Android.js

--- Android.js
+++ Android.js
@@ -1,107 +1,96 @@
 const wd = require("wd");
-
 /**
  * Username to be used for running the test.
  */
 const username = process.env.LT_USERNAME || "username";
-
 /**
  * The access key to be used for running test test.
  */
 const accessKey = process.env.LT_ACCESS_KEY || "accessKey";
 
-const lt_devices = process.env.LT_DEVICES;
+const buildName = process.env.LT_BUILD_NAME;
+const appId = process.env.LT_APP_ID;
+const deviceName = process.env.LT_DEVICE_NAME;
+const deviceVersion = process.env.LT_DEVICE_VERSION;
 
 /**
-   * Run an android test.
-   */
-    async function runAndroidTest() {
-      const driver = wd.promiseRemote(
-          `https://${username}:${accessKey}@mobile-hub.lambdatest.com/wd/hub`
-        );
+ * Capabilities to be passed while running the test.
+ */
+const desiredCapabilities = {
+  app: appId, // Enter the 'app_url' here.
+  build: buildName,
+  name: "Sample Test NodeJS",
+  deviceName: deviceName,
+  isRealMobile: true,
+  platformName: "android",
+  platformVersion: deviceVersion,
+  video: true,
+  visual: true,
+};
 
-      const DEFAULT_TIMEOUT = 10000;
-      try {
-        driver
-          .init(desiredCapabilities)
-          .then(function () {
-            return driver.waitForElementById("color", DEFAULT_TIMEOUT);
-          })
-          .then(function (colorButton) {
-            return colorButton.click();
-          })
-          .then(function () {
-            return driver.waitForElementById("Text", DEFAULT_TIMEOUT);
-          })
-          .then(function (text) {
-            text.click();
-            return driver.waitForElementById("toast", DEFAULT_TIMEOUT);
-          })
-          .then(function (toast) {
-            toast.click();
-            return driver.waitForElementById("notification", DEFAULT_TIMEOUT);
-          })
-          .then(function (notification) {
-            notification.click();
-            return driver.waitForElementById("geoLocation", DEFAULT_TIMEOUT);
-          })
-          .then(function (geoLocation) {
-            geoLocation.click();
-            return driver.waitForElementById("buttonPage", DEFAULT_TIMEOUT);
-          })
-          .then(function (Home) {
-            Home.click();
-            return driver.waitForElementById("speedTest", DEFAULT_TIMEOUT);
-          })
-          .then(function (speedTest) {
-            speedTest.click();
-            return driver.waitForElementById("webview", DEFAULT_TIMEOUT);
-          })
-          .then(function (Browser) {
-            Browser.click();
-            return driver.waitForElementById("url", DEFAULT_TIMEOUT);
-          })
-          .then(function (url) {
-            url.type("https://www.lambdatest.com");
-            return driver.waitForElementById("find", DEFAULT_TIMEOUT);
-          })
-          .then(function (find) {
-            find.click();
-            console.log("Test completed");
-            driver.quit();
-          });
-      } catch (e) {
+console.log("buildName ", buildName);
+console.log("appId ", appId);
+
+const driver = wd.promiseRemote(
+  `https://${username}:${accessKey}@mobile-hub.lambdatest.com/wd/hub`
+);
+const DEFAULT_TIMEOUT = 10000;
+/**
+ * Run an android test.
+ */
+async function runAndroidTest() {
+  try {
+    driver
+      .init(desiredCapabilities)
+      .then(function () {
+        return driver.waitForElementById("color", DEFAULT_TIMEOUT);
+      })
+      .then(function (colorButton) {
+        return colorButton.click();
+      })
+      .then(function () {
+        return driver.waitForElementById("Text", DEFAULT_TIMEOUT);
+      })
+      .then(function (text) {
+        text.click();
+        return driver.waitForElementById("toast", DEFAULT_TIMEOUT);
+      })
+      .then(function (toast) {
+        toast.click();
+        return driver.waitForElementById("notification", DEFAULT_TIMEOUT);
+      })
+      .then(function (notification) {
+        notification.click();
+        return driver.waitForElementById("geoLocation", DEFAULT_TIMEOUT);
+      })
+      .then(function (geoLocation) {
+        geoLocation.click();
+        return driver.waitForElementById("buttonPage", DEFAULT_TIMEOUT);
+      })
+      .then(function (Home) {
+        Home.click();
+        return driver.waitForElementById("speedTest", DEFAULT_TIMEOUT);
+      })
+      .then(function (speedTest) {
+        speedTest.click();
+        return driver.waitForElementById("webview", DEFAULT_TIMEOUT);
+      })
+      .then(function (Browser) {
+        Browser.click();
+        return driver.waitForElementById("url", DEFAULT_TIMEOUT);
+      })
+      .then(function (url) {
+        url.type("https://www.lambdatest.com");
+        return driver.waitForElementById("find", DEFAULT_TIMEOUT);
+      })
+      .then(function (find) {
+        find.click();
+        console.log("Test completed");
         driver.quit();
-      }
-      console.log("Test is running....");
-    }
-
-const devices = JSON.parse(lt_devices);
-
-for (let i = 0; i < devices.length; i++) {
-  
-  const buildName = process.env.LT_BUILD_NAME;
-  const appId = devices[i].appId;
-  const deviceName = devices[i].deviceName;
-  const deviceVersion = devices[i].deviceVersion;
-  const platformName = devices[i].platformName;
-  
-  console.log("buildName : ", buildName);
-  console.log("this caps : ", devices[i]);
-   /**
-   * Capabilities to be passed while running the test.
-   */
-  const desiredCapabilities = {
-    app: appId, // Enter the 'app_url' here.
-    build: buildName,
-    name: "Sample Test NodeJS",
-    deviceName: deviceName,
-    isRealMobile: true,
-    platformName: platformName,
-    platformVersion: deviceVersion,
-    video: true,
-    visual: true,
-  };
-
-  runAndroidTest();
-} 
+      });
+  } catch (e) {
+    driver.quit();
+  }
+  console.log("Test is running....");
+}
+runAndroidTest();
